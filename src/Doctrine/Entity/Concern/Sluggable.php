fa1a1c966d929e5ee1a9f652909299f29601b211
Fredxd <frederic.baudouin.45@gmail.com>
2021-09-13 17:16:52 +0200

    feat(concern): add sluggable

---
+++ src/Doctrine/Entity/Concern/Sluggable.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppoloDev\SFToolbox\Doctrine\Entity\Concern;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+
+trait Sluggable
+{
+    #[ORM\Column(type: Types::STRING, length: 255, nullable: true)]
+    private ?string $slug = null;
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    #[ORM\PrePersist]
+    #[ORM\PreUpdate]
+    public function updateSlug(): void
+    {
+        if (method_exists($this, 'getTitle')) {
+            $slugger = new AsciiSlugger();
+            $this->setSlug(strtolower($slugger->slug($this->getTitle())));
+        }
+    }
+}
