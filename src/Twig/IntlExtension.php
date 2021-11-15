bb9cb144338566c6019f7df2e284a67e0dc4f19e
Fredxd <frederic.baudouin.45@gmail.com>
2021-11-15 18:16:37 +0100

    feat(twig): add Intl extension

---
+++ src/Twig/IntlExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppoloDev\SFToolbox\Twig;
+
+use IntlDateFormatter;
+use RuntimeException;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class IntlExtension extends AbstractExtension
+{
+    public function __construct()
+    {
+        if (!class_exists(IntlDateFormatter::class)) {
+            throw new RuntimeException('The native PHP intl extension (http://php.net/manual/en/book.intl.php) is needed to use intl-based filters.');
+        }
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('localizeddate', [$this, 'localizeddate']),
+        ];
+    }
+
+    public function localizeddate(
+        \DateTime $date,
+        string $format = null,
+        string $locale = 'fr-FR',
+        string $timeZone = 'Europe/Paris',
+        bool $ucFirst = true
+    ): string {
+        $formatter = IntlDateFormatter::create(
+            $locale,
+            IntlDateFormatter::FULL,
+            IntlDateFormatter::FULL,
+            $timeZone,
+            IntlDateFormatter::GREGORIAN,
+            $format
+        );
+
+        $formattedDate = $formatter->format($date->getTimestamp());
+
+        if(!$ucFirst) {
+            return $formattedDate;
+        }
+
+        return implode(' ', array_map(fn ($item) => ucfirst($item), explode(' ', $formattedDate)));
+    }
+}
