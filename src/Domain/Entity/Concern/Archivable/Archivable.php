271dba6785cdc64f3580e1410a60612756e544af
Fredxd <frederic@appolo.fr>
2023-09-11 14:22:34 +0200

    add Archivable trait

---
+++ src/Domain/Entity/Concern/Archivable/Archivable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppoloDev\SFToolboxBundle\Domain\Entity\Concern\Archivable;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+trait Archivable
+{
+    #[ORM\Column(type: Types::BOOLEAN)]
+    #[Groups(['archive'])]
+    private bool $archived = false;
+
+    public function isArchived(): bool
+    {
+        return $this->archived;
+    }
+
+    public function setArchived(bool $archived): self
+    {
+        $this->archived = $archived;
+
+        return $this;
+    }
+}
