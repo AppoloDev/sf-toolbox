394187befc5569e71aa62c961f1089ef55c7f53b
arnaud <arnaud@appolo.fr>
2025-03-24 13:54:54 +0100

    Wip:

--- assets/js/components/tomselect/tomselect.js
+++ assets/js/components/tomselect/tomselect.js
@@ -1,41 +1,48 @@
 import TomSelect from "tom-select";
 import maxItemPlugin from "./plugins/max-items";
-// TODO: I18n
+
 class Autocomplete extends HTMLElement {
     connectedCallback() {
         const select = this.querySelector('select') || this.querySelector('input');
 
-        if (select) {
-            let options = {
-                plugins: ['dropdown_input'],
-                maxOptions: null,
-                create: this.hasAttribute('create'),
-                dropdownParent: 'body',
-                maxItemsCount: parseInt(this.getAttribute('maxItems')) ?? 2,
-                render: {
-                    no_results: function (data, escape) {
-                        return '<div class="no-results">Aucun résultat trouvé</div>';
-                    },
-                }
-            };
+        const conf = JSON.parse(this.getAttribute('options'));
 
-            if (this.hasAttribute('options')) {
-                options.options = JSON.parse(this.getAttribute('options')).map(e => ({
+        const addText = this.getAttribute('addText') ?? 'Add';
+        const noMatchText = this.getAttribute('noMatchText') ?? 'No matches found';
+
+        if (select) {
+            let options = {...{
+                    plugins: ['dropdown_input'],
+                    maxOptions: null,
+                    maxItems: conf.maxItems ?? null,
+                    create: this.hasAttribute('create'),
+                    dropdownParent: 'body',
+                    render: {
+                        option_create: function( data, escape ){
+                            return `<div class="create">${addText} : <strong>${escape(data.input)}</strong>&hellip;</div>`;
+                        },
+                        no_results: function (data, escape) {
+                            return `<div class="no-results">${noMatchText}</div>`;
+                        },
+                    }
+                }, ...conf};
+
+            if (conf.options) {
+                options.options = options.options.map(e => ({
                     text: e,
                     value: e
                 }));
             }
 
-            if (select.hasAttribute('multiple')) {
+            if (conf.maxItems === null || conf.maxItems > 1) {
                 options.plugins.push('remove_button');
                 options.plugins.push('checkbox_options');
             }
 
-
-            if (this.hasAttribute('maxItems')) {
+            if (conf.maxItemsCount) {
                 options.plugins.push('maxItemPlugin');
-                if (parseInt(this.getAttribute('maxItems')) === 1) {
-                    options.maxItems = 1;
+                if(conf.maxItemsCount) {
+                    options.maxItemsCount = conf.maxItemsCount ?? null;
                 }
             }
 
--- src/Form/FormType/TomSelectType.php
+++ src/Form/FormType/TomSelectType.php
@@ -2,14 +2,34 @@
 
 namespace AppoloDev\SFToolboxBundle\Form\FormType;
 
+use AppoloDev\SFToolboxBundle\Form\DataTransformer\StringToArrayTransformer;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TomSelectType extends AbstractType
 {
+
     public function getParent(): string
     {
-        return ChoiceType::class;
+        return TextType::class;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        if($options['multiple']) {
+            $builder->addModelTransformer(new StringToArrayTransformer());
+        }
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+        $options['configuration']['maxItems'] = $options['configuration']['maxItems'] ?? ($options['multiple'] ? null : 1);
+        $view->vars['configuration'] = $options['configuration'];
     }
 
     public function getBlockPrefix(): string
@@ -17,5 +37,15 @@ class TomSelectType extends AbstractType
         return 'tom_select';
     }
 
-    // TODO: Option resolver pour l'attribut . Nom à définir (Options ?) + Passer la local pour i18n
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'configuration' => [],
+            'multiple' => false,
+        ]);
+
+        $resolver->setAllowedTypes('configuration', ['array']);
+        $resolver->setAllowedTypes('multiple', ['bool']);
+    }
 }
