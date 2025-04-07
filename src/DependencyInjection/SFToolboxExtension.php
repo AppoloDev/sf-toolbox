bd14ace8c6daa490e6c6e63737e64b1801c2e9c0
arnaud <arnaud@appolo.fr>
2025-04-07 14:32:03 +0200

    Fix: service twig component config

--- src/DependencyInjection/SFToolboxExtension.php
+++ src/DependencyInjection/SFToolboxExtension.php
@@ -49,7 +49,19 @@ class SFToolboxExtension extends Extension implements PrependExtensionInterface
         if (isset($bundles['TwigComponentBundle'])) {
             $container->prependExtensionConfig('twig_component', [
                 'defaults' => [
-                    'AppoloDev\SFToolboxBundle\UI\\' => '@SFToolboxBundle/ui'
+                    'AppoloDev\SFToolboxBundle\UI\Atoms\Alert\\' => '@SFToolboxBundle/ui/atoms/alert/',
+                    'AppoloDev\SFToolboxBundle\UI\Atoms\Badge\\' => '@SFToolboxBundle/ui/atoms/badge/',
+                    'AppoloDev\SFToolboxBundle\UI\Atoms\Button\\' => '@SFToolboxBundle/ui/atoms/button/',
+                    'AppoloDev\SFToolboxBundle\UI\Atoms\Toast\\' => '@SFToolboxBundle/ui/atoms/toast/',
+                    'AppoloDev\SFToolboxBundle\UI\Molecules\Card\\' => '@SFToolboxBundle/ui/molecules/card/',
+                    'AppoloDev\SFToolboxBundle\UI\Molecules\Form\\' => '@SFToolboxBundle/ui/molecules/form/',
+                    'AppoloDev\SFToolboxBundle\UI\Molecules\Generic\\' => '@SFToolboxBundle/ui/molecules/generic/',
+                    'AppoloDev\SFToolboxBundle\UI\Molecules\List\\' => '@SFToolboxBundle/ui/molecules/list/',
+                    'AppoloDev\SFToolboxBundle\UI\Organisms\Dropdown\\' => '@SFToolboxBundle/ui/organisms/dropdown/',
+                    'AppoloDev\SFToolboxBundle\UI\Organisms\User\\' => '@SFToolboxBundle/ui/organisms/user/',
+                    'AppoloDev\SFToolboxBundle\UI\Templates\Card\\' => '@SFToolboxBundle/ui/templates/card/',
+                    'AppoloDev\SFToolboxBundle\UI\Templates\Form\\' => '@SFToolboxBundle/ui/templates/form/',
+                    'AppoloDev\SFToolboxBundle\UI\Templates\List\\' => '@SFToolboxBundle/ui/templates/list/',
                 ],
             ]);
         }
